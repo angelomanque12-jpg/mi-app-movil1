d824a78f669a07da206d8c59a4a2c2c149a999cf
Angelo-mx2 <AN.MANQUE@DUOCUC.CL>
2025-09-03 09:21:11 -0400

    clase 03

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, CommonModule]
 })
-export class HomePage {
-  username = '';
+export class HomePage implements OnInit {
+  username: string = '';
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {
     this.username = this.userService.getUsername();
   }
 
+  goToPerfil() {
+    this.router.navigate(['/perfil']);
+  }
+
+  goToConfig() {
+    this.router.navigate(['/configuracion']);
+  }
+
   logout() {
-    this.userService.logout();
+    this.userService.setUsername('');
     this.router.navigate(['/login']);
   }
 }
