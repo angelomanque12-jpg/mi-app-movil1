d7b85bdebd3c0771c6d86b6ea2019d88c8393949
Angelo-mx2 <239900111+angelomanque12-jpg@users.noreply.github.com>
2025-11-08 18:53:11 -0300

    Guard
    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
-  { path: 'lugares', loadComponent: () => import('./lugares/lugares.page').then(m => m.LugaresPage) },
-  { path: 'capture', loadChildren: () => import('./pages/capture/capture.module').then(m => m.CapturePageModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule), canActivate: [authGuard] },
+  { path: 'lugares', loadComponent: () => import('./lugares/lugares.page').then(m => m.LugaresPage), canActivate: [authGuard] },
+  { path: 'capture', loadChildren: () => import('./pages/capture/capture.module').then(m => m.CapturePageModule), canActivate: [authGuard] },
   {
     path: 'place-detail/:id',
-    loadChildren: () => import('./pages/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
+    loadChildren: () => import('./pages/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule),
+    canActivate: [authGuard]
   },
   {
     path: '**',
